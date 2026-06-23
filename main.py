4dcc94dec64733222cfba1266d42fafd41015848
Mangesh Chavan <mangesh.chavan0022@gmail.com>
2026-06-23 16:40:34 +0530

    added interviews

--- main.py
+++ main.py
@@ -8,6 +8,10 @@ app = FastAPI()
 llm = LLMClient("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")
 
 
+class InterviewRequest(BaseModel):
+    question: str
+
+
 class ChatRequest(BaseModel):
     prompt: str
 
@@ -32,4 +36,30 @@ def chat(request: ChatRequest):
 # def chat(prm):
 #     prompt="what is 3+3?"
 #     return llm.ask("what is 3+3?")
-    
+
+
+@app.post("/interview") 
+def interview(request:InterviewRequest):
+    question=request.question
+
+    prompt=f"""
+    You are a senior Python developer and interview coach. You will be given a question from a candidate. You will provide a detailed answer to the question, explaning the reasoning behind your answer. You will also provide a list of follow-up questions that the candidate can ask to further explore the topic. The follow-up questions should be open-ended and encourage critical thinking.
+
+    Input:
+    Question: {question}
+    Answer: you will give the answer.
+
+    Output:
+        Provide:
+        1. Answer
+        2. Conceptual Diagram realted to the concept asked in the question
+        3. Difficuly level
+        4. Follow-up questions
+    """
+
+    answer=llm.ask(prompt)
+
+    return {
+        "question": request.question,
+        "response": answer
+    }
